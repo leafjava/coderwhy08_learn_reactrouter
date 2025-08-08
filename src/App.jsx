1eb1d718475cad4c458d8f1d2f5845a248fea41e
LAPTOP-KCF3SC5G\HUAWEI <a894074295@126.com>
2025-08-08 14:16:08 +0800

    1566_day87_Redux核心原理-React路由-Hooks-05_(掌握)Router-React的Router基本使用

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
 import React, { PureComponent } from 'react'
+import {Route,Routes, Link} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
 
 export class App extends PureComponent {
   render() {
     return (
-      <div>App</div>
+      <div className='app'>
+        <div className='header'>
+          <span>Header</span>
+          <div className='nav'>
+            <Link to="/home">首页</Link>
+            <Link to="/about">关于</Link>
+          </div>
+          <hr/>
+        </div>
+        <div className='content'>
+          {/* 映射关系:path => Component */}
+
+          <Routes>
+            {/* <Route path='/' element={<Home/>}></Route> */}
+            <Route path='/home' element={<Home/>}></Route>
+            <Route path='/about' element={<About/>}></Route>
+          </Routes>
+        </div>
+        <div className='footer'>
+          <hr/>
+          Footer
+        </div>
+      </div>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import ReactDOM from "react-dom/client"
+import { StrictMode } from "react"
+import {BrowserRouter,HashRouter} from "react-router-dom"
 
 import App from './App.jsx'
 
 const root = ReactDOM.createRoot(document.querySelector("#root"))
 
-root.render(<App/>)
+root.render(
+  <StrictMode>
+    <HashRouter>
+      <App/>
+    </HashRouter>
+  </StrictMode>
+  
+)
